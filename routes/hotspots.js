37292063ac29939a427e3c8add12e000e71b26ee
Yimni Chan <yimni.chan.yn@gmail.com>
2020-06-16 15:58:47 -0400

    add query way

--- routes/hotspots.js
+++ routes/hotspots.js
@@ -1,18 +1,23 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const mongoose = require("mongoose").mongoClient;
 const router = express.Router();
 
 const HotspotModel = mongoose.model('hotspotModel');
 
 // Route to serve user
-router.get("/", async (req, res, next) => {
-});
+
+ router.get("/", async (req, res, next) => {
+
+    const query = HotspotModel.find().
+    Where('zipcode'.regec(req.value));
+//     res.render('/zipcode', {retrievedData : router.get('data')});
+    query.getFilter();
+    
+ });
 
 // Route to handle adding a user
 router.post("/", async (req, res, next) => {
-
-
-
+    router.set('data',req.body.HotspotModel)
 });
 
 // Route to handle edit a user

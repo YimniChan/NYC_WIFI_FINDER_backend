fad5608a4c76b212abb71a182c4bce843b2dab68
Yimni Chan <yimni.chan.yn@gmail.com>
2020-06-16 11:20:12 -0400

    add sone model schema to hotspotModel

--- models/hotspotModel.js
+++ models/hotspotModel.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const hotsportModel = new mongoose.Schema({
-  name: { type: String, required: true },
+const hotsportModel = new Schema({
+  name: { type: String, required: true, unique: true },
   address: { type: String, required: true },
-  zipCode: { type: Number(5)},
-  // 
-  //rating: { type: float, min: 0.00, max: 5.00},
+  zipCode: { type: Number,  required: true },
+  /*  zipCode 5 digit limit add from front end
+  location: {
+    streetAddress: { type: String, required: true, unqiue: true},
+    city: { type: String, required: true}, 
+    state: { type: String, required: true},
+    zipCode: { type: Number, required: true} 
+  },
+  rating: { type: float, min: 0.00, max: 5.00},
   //add the write in time
+  */
 });
-
+  
 module.exports = mongoose.model('hotsportModel',hotsportModel);

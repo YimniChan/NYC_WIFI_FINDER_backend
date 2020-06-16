c9f9af0f2e85a31f69d92567790f7cf4e0ce3864
Bayardo Zelaya <bayardozelaya@gmail.com>
2020-06-16 11:15:40 -0400

    pull error

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-app.listen(5001, function() {
-    console.log('listening on 5001')
-  })
+app.listen(5001, function () {
+  console.log("listening on 5001");
+});

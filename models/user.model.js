89d6b5a9d212492af642b96a3e4ee22aaa4b345d
VICTOR LI <victor.li.1995@hotmail.com>
2020-06-16 20:29:18 -0400

    uncommented out fields in User schema

--- models/user.model.js
+++ models/user.model.js
@@ -5,9 +5,9 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   firstName: { type: String, required:false },
   lastName: { type: String, required: false },
-  // email: { type: String, required: true },
-  // password: { type: String, min: 8, max: 20, required: true },
-  //password encryption
+  email: { type: String, required: true },
+  password: { type: String, min: 8, max: 20, required: true },
+  // password encryption
 });
 
 const User = mongoose.model("User", userSchema);

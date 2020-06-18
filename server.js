6e07b5eeeb4868251a3125526fd995cd0f8ce338
Yimni Chan <yimni.chan.yn@gmail.com>
2020-06-18 13:24:23 -0400

    command a line

--- server.js
+++ server.js
@@ -31,9 +31,8 @@ mongoose
   .then(() => console.log("mongoDB Connected..."))
   .catch((err) => console.log(err));
 
-
 // Parsing requests into JSON
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 
 
 //require router files
@@ -48,4 +47,4 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
-module.exports = app;
+//module.exports = app;

221499348a5cff2231820abe534b081a9df8956d
Yimni Chan <yimni.chan.yn@gmail.com>
2020-06-18 11:57:39 -0400

    add a connect to model

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+//const path = require('path');
 
 const app = express();
 
@@ -12,16 +13,30 @@ app.use(bodyParser.json());
 dotenv.config();
 const mongoDB = process.env.MONGO_URI;
 
+if (!mongoDB) {
+  throw new Error('You must provide a string to connect to MongoDB Atlas');
+}
+//connect to model
+require("./models/hotSpot.model");
+require("./models/user.model");
+
 //connect to mongo
 mongoose
   .connect(mongoDB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
+//    useFindAndModify: false,
+//    useCreateIndex: true,
   })
   .then(() => console.log("mongoDB Connected..."))
   .catch((err) => console.log(err));
+
+// Adding cors middleware to app to relax CORS error
+app.use(cors());
+
+// Parsing requests into JSON
+app.use(bodyParser.json());
+
 
 //require router files
 const hotSpotsRouter = require("./routes/hotspots");
@@ -34,3 +49,5 @@ app.use("/users", usersRouter);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
+
+module.exports = app;

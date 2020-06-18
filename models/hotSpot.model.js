79418e73fcd2db4d10f083317e19a0132a233120
Yimni Chan <yimni.chan.yn@gmail.com>
2020-06-18 10:29:06 -0400

    change the variable in db

--- models/hotSpot.model.js
+++ models/hotSpot.model.js
@@ -2,13 +2,22 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const hotSpotSchema = new Schema({
-  name: { type: String, required: true, unique: true },
-  address: { type: String, required: true },
-  zipCode: { type: Number, required: true },
-  /*   type:{
-    free: {type: Boolean},
-    limitFree: { type: Boolean},
-  },
+  borough:{ type: String, required: true }, 
+  type: { type: String, required: true }, 
+  provider: { type: String, required: true }, 
+  name: { type: String, required: true },
+  location:{ type: String, required: true }, 
+  city: { type: String, required: true }, 
+  ssid: { type: String, required: true }, 
+  boroughName: { type: String, required: true },
+  NeighborhoodTA:{ type: String, required: true }, 
+  zipcode: { type: Number, required: true }, 
+  latitude: { type: Float32Array },
+  longitudes: { type: Float32Array },
+  // name: { type: String, required: true, unique: true },
+  // address: { type: String, required: true },
+  // type:{type: String, }
+  /*  
   zipCode 5 digit limit add from front end
   location: {
     streetAddress: { type: String, required: true, unique: true},
@@ -16,8 +25,7 @@ const hotSpotSchema = new Schema({
     state: { type: String, required: true},
     zipCode: { type: Number, required: true}
   },
-  rating: { type: float, min: 0.00, max: 5.00},
-  //add the write in time
+
   */
 });
 

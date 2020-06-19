47ec872b15ad13878b0350f11598962fec691439
VICTOR LI <victor.li.1995@hotmail.com>
2020-06-19 01:34:49 -0400

    implemented add,delete,get in hotspot routes

--- routes/hotspots.js
+++ routes/hotspots.js
@@ -2,46 +2,60 @@ const express = require("express");
 const router = express.Router();
 const hotSpot = require("../models/hotSpot.model");
 
-console.log("cat");
 
-//find all current hotSpot locations
+//Find location
 router.get("/", (req, res) => {
     hotSpot
       .find()
       .then((hotSpots) => res.json(hotSpots))
       .catch((err) => res.status(400).json("Error: " + err));
-//  res.send({ toy: 213 });
 });
 
 
-router.post("/add",(req, res) => {
-  const hotSpotName = req.body.Name;
-  const hotSpotLocation = req.body.Location;
-  const hotSpotCity = req.body.City;
-  const hotSpotSsid = req.body.SSID;
-  const hotSpotZipcode = req.body.ZipCode;
-  const hotSpotLatitude = req.body.latitude;
-  const hotSpotLongitudes = req.body.longitudes; 
-  // const hotSpotBorough = req.body.borough;
-  // const hotSpotType = req.body.type;
-  // const hotSpotProvider = req.body.provider;
-  // const hotSpotBoroughName = req.body.boroughName;
-  // const hotSpotNeighbor = req.body.NeighborhoodTA;
 
+//Add location
+router.post("/add",(req, res) => {
+
+  const {name, location, city, 
+        boroughName, zipcode, latitude,
+        longtiudes, type, provider,
+        ssid} = req.body;
+ 
   const newHotSpot = new hotSpot({
-   hotSpotName,
-   hotSpotLocation,
-   hotSpotCity,
-   hotSpotSsid,
-   hotSpotZipcode,
-   hotSpotLatitude,
-   hotSpotLongitudes,
+  name,
+  location,
+  city,
+  boroughName,
+  zipcode,
+  latitude,
+  longtiudes,
+  type,
+  provider,
+  ssid
   });
+
   newHotSpot
       .save()
-      .then(() => res.json(newHotSpot)
-      .catch((err) => res.statusMessage(400).json("Error: " +err));
+      .then(() => res.json(newHotSpot))
+      .catch((err) => res.statusMessage(400).json("Error: " +err))
 
 })
+
+
+//Delete location based on the latitude
+router.delete("/delete", (req, res) =>{
+
+          const{latitude} = req.body;
+
+          try{
+            hotSpot.findOne({latitude}).deleteOne().exec();
+            res.json("Deleted location")
+          }
+          catch(err){
+            res.status(400).json("Error: " + err);
+          }
+})
+
+
 
 module.exports = router;

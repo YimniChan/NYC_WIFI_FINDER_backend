b99e98df1f79f27d9c33f1f922bc26697ef33d83
VICTOR LI <victor.li.1995@hotmail.com>
2020-06-19 00:26:41 -0400

    fixed hotspot model

--- models/hotSpot.model.js
+++ models/hotSpot.model.js
@@ -2,12 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const hotSpotSchema = new Schema({
-  // borough:{ type: String, required: false },
+ 
   name: { type: String, required: true },
   location:{ type: String, required: true }, 
   city: { type: String, required: true }, 
-  boroughName: { type: String, required: true },
-  NeighborhoodTA:{ type: String, required: true }, 
+  boroughName: {type: String, required: true},
   zipcode: { type: Number, required: true }, 
   latitude: { type: Number },
   longitudes: { type: Number },

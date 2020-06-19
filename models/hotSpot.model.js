8566778ea2807dc6a7d2082694c29519acaff2c4
VICTOR LI <victor.li.1995@hotmail.com>
2020-06-18 22:38:47 -0400

    populating database from api json

--- models/hotSpot.model.js
+++ models/hotSpot.model.js
@@ -2,20 +2,23 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const hotSpotSchema = new Schema({
-  borough:{ type: String, required: true }, 
-  type: { type: String, required: true }, 
-  provider: { type: String, required: true }, 
+  // borough:{ type: String, required: false },
   name: { type: String, required: true },
   location:{ type: String, required: true }, 
   city: { type: String, required: true }, 
-  ssid: { type: String, required: true }, 
   boroughName: { type: String, required: true },
   NeighborhoodTA:{ type: String, required: true }, 
   zipcode: { type: Number, required: true }, 
   latitude: { type: Number },
   longitudes: { type: Number },
+  type: { type: String, required: true }, 
+  provider: { type: String, required: true }, 
+  ssid: { type: String, required: true }, 
 
 });
+
+
+
 
 const hotSpots = mongoose.model("hotSpot", hotSpotSchema);
 
